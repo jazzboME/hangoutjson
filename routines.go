2ecf796e1ed2831c4bbe4d7a6febc5334068c9d9
Kevin Foss <kevin.foss@gmail.com>
2017-08-28 00:09:56 -0400

    Add routine to decode 16 digit timestamp to Time type

--- routines.go
+++ routines.go
@@ -2,10 +2,24 @@ package hangoutjson
 
 import (
 	"encoding/json"
+	"time"
+	"strconv"
 )
 
 func Do(bytestream []byte) (Hangouts, int, error) {
 	var jsontree Hangouts
 	err := json.Unmarshal(bytestream, &jsontree)
 	return jsontree, len(jsontree.ConversationState), err
+}
+
+func ConvTimestamp(timestamp string) (time.Time) {
+	secs, err := strconv.ParseInt(timestamp[0:10], 10, 64)
+	if err != nil {
+		secs = 0
+	}
+	msecs, err := strconv.ParseInt(timestamp[10:16], 10, 64)
+	if err != nil {
+		msecs = 0
+	}
+	return time.Unix(secs,msecs)
 }
