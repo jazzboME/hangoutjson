72f12aaffa5092f17f0c9a1c94143675cf111015
Kevin Foss <kevin.foss@gmail.com>
2017-09-02 22:54:30 -0400

    Add comments for routines

--- routines.go
+++ routines.go
@@ -6,12 +6,15 @@ import (
 	"strconv"
 )
 
+// Do returns unmarshaled Hangouts stucture 
+// from bytesteam (i.e. file)
 func Do(bytestream []byte) (Hangouts, int, error) {
 	var jsontree Hangouts
 	err := json.Unmarshal(bytestream, &jsontree)
 	return jsontree, len(jsontree.ConversationState), err
 }
 
+// ConvTimestamp converts a 16 digit timestamp to a time.
 func ConvTimestamp(timestamp string) (time.Time) {
 	secs, err := strconv.ParseInt(timestamp[0:10], 10, 64)
 	if err != nil {
